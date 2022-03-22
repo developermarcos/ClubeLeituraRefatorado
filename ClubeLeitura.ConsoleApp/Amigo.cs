d320a1014e45f3dcfd4d094ccd8a30a530a3360f
Marcos Lima <developermarcos@outlook.com>
2022-03-22 15:05:13 -0300

    Add project files.

---
+++ ClubeLeitura.ConsoleApp/Amigo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ClubeLeitura.ConsoleApp
+{
+    public class Amigo
+    {
+        public int numero;
+        public string nome;
+        public string responsavel;
+        public string telefone;
+        public string endereco;
+    }
+}
